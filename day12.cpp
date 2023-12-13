8569500255cbaa8b3b3c6ee599ceb6549fdd3428
Fredrik Hagman <fhagman@spotify.com>
2023-12-13 08:54:31 +0100

    day13

--- day12.cpp
+++ day12.cpp
@@ -1,12 +1,68 @@
 #include "aoc.h"
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
+//???.### 1,1,3
+
+struct Spring {
+  std::string record;
+  std::vector<int> groups;
+};
+
+Spring parseSpring(const std::string &line) {
+  const auto sp = aoc::split(line, " ");
+  return {sp[0], aoc::intvec(aoc::split(sp[1], ","))};
+}
+
+int getUnknown(const std::string& record) {
+  return std::accumulate(record.begin(), record.end(), 0, [] (int acc, char c) { return c == '?' ? acc + 1 : acc; });
+}
+
+bool match(int num, const Spring &spring) {
+  int c = 0;
+  bool in_group = false;
+  std::vector<int> groups;
+  for (char l : spring.record) {
+    if(l == '?') {
+      l =  (num & 1 << c++) ? '.' : '#';
+    }
+    if (l == '#') {
+      if (in_group) {
+        ++groups.back();
+      } else {
+        groups.push_back(1);
+        in_group = true;
+      }
+    } else {
+      in_group = false;
+    }
+  }
+  if (groups.size() == spring.groups.size()) {
+    return std::equal(groups.begin(), groups.end(), spring.groups.begin());
+  }
+  return false;
+}
+
+int arrangements(int acc, const Spring& spring) {
+  const int unknown = getUnknown(spring.record);
+  const uint64_t mutations = 1 << unknown;
+//  std::cout << spring.record << " has " << unknown << " unknown and has " << mutations << " mutations.\n";
+  for (int i = 0; i != mutations; ++i) { 
+    if (match(i, spring)) {
+      ++acc;
+    }
+  }
+  return acc;
+}
 
 int main() {
-  aoc::foreach("day12.input", [] (std::string line) {
-    std::cout << line << std::endl;
+  std::vector<Spring> springs;
+  aoc::foreach("day12.input", [&springs] (std::string line) {
+    springs.push_back(parseSpring(line));
   });
-  std::cout << "part 1: " << "" << std::endl;
+  std::cout << "part 1: " << std::accumulate(springs.begin(), springs.end(), 0, arrangements) << std::endl;
   std::cout << "part 2: " << "" << std::endl;
   return 0;
 }
--- day13.cpp
+++ day13.cpp
@@ -1,15 +1,76 @@
 #include "aoc.h"
 #include <iostream>
+#include <numeric>
+#include <vector>
+#include <algorithm>
 
+std::vector<std::string> rotate(const std::vector<std::string>& grid) {
+  int len = grid.front().length();
+  std::vector<std::string> res(len, std::string(""));
+  for (int c = 0 ; c != grid.size(); ++c) {
+    for(int r = 0; r != len; ++r) {
+      res[r] += grid[c][r];
+    }
+  }
+  return res;
+}
 
-int main() {
-  aoc::foreach("day12.input", [] (std::string line) {
-    std::cout << line << std::endl;
-  });
-  std::cout << "part 1: " << "" << std::endl;
-  std::cout << "part 2: " << "" << std::endl;
+bool reflection(const std::vector<std::string>& grid, int line, int tol) {
+  std::vector<std::string> lo(grid.begin(), grid.begin() + line);
+  std::vector<std::string> hi(grid.begin() + line, grid.end());
+  auto len = std::min(lo.size(), hi.size());
+  std::reverse(lo.begin(), lo.end());
+  lo.resize(len);
+  hi.resize(len);
+  int smudge = 0;
+  for (int i = 0; i != len; ++i) {
+    for (int j = 0; j != lo[i].length(); ++j) {
+      if (lo[i][j] != hi[i][j]) {
+        ++smudge;
+      }
+    }
+  }
+  return smudge == tol;
+}
+
+int reflectRow(const std::vector<std::string>& grid, int tol) {
+  for (int i = 1; i < grid.size(); ++i) {
+    if (reflection(grid, i, tol)) {
+      return i;
+    }
+  }
   return 0;
 }
 
-// part 1: 
-// part 2: 
+int score(const std::vector<std::string>& grid, int tol) {
+  if (int row = reflectRow(grid, tol)) {
+    return 100 *row;
+  }
+  return reflectRow(rotate(grid), tol);
+}
+
+int score1(int acc, const std::vector<std::string>& grid) {
+  return acc + score(grid, 0);
+}
+
+int score2(int acc, const std::vector<std::string>& grid) {
+  return acc + score(grid, 1);
+}
+
+int main() {
+  std::vector<std::vector<std::string>> grids = {std::vector<std::string>()};
+  aoc::foreach("day13.input", [&grids] (std::string line) {
+    if(line.empty()) {
+      grids.emplace_back(std::vector<std::string>());
+    } else {
+      grids.back().push_back(line);
+    }
+  });
+
+  std::cout << "part 1: " << std::accumulate(grids.begin(), grids.end(), 0, score1) << std::endl;
+  std::cout << "part 2: " << std::accumulate(grids.begin(), grids.end(), 0, score2) << std::endl;
+  return 0;
+}
+
+// part 1: 30158
+// part 2: 36474
